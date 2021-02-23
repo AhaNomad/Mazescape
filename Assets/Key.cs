bcb3a3c962a1a515bdd1f775b5c407625e6bda71
Ejler2 <57224628+Ejler2@users.noreply.github.com>
2021-02-23 15:50:44 +0100

    key

---
+++ Assets/Key.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public GameControl GC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Keyscript ready");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            GC.AddRedkey();
+        }
+    }
+
+}
